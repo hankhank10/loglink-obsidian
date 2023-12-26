f84b3020328ca517e5ae3446a9e0436fb8722359
hankhank10 <m.a.hankinson@gmail.com>
2023-12-26 09:44:24 +0000

    Update

--- main.ts
+++ main.ts
@@ -1,77 +1,137 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface LoglinkSettings {
+	token: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: LoglinkSettings = {
+	token: ''
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class LoglinkSettingTab extends PluginSettingTab {
+	private plugin: Plugin;
+
+	constructor(app: App, plugin: Plugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		let {containerEl} = this;
+
+		containerEl.empty();
+		containerEl.createEl('h1', {text: 'Settings for Loglink plugin'});
+
+		containerEl.createEl('p', {text: 'LogLink is a service that allows you to send messages to Obsidian from Telegram.'});
+		containerEl.createEl('p', {text: 'Full instructions for setting up LogLink are available at:'});
+		containerEl.createEl('a', {text: 'https://loglink.it', href: 'https://loglink.it'});
+		containerEl.createEl('p', {text: ''});
+
+
+		new Setting(containerEl)
+			.setName('Token')
+			.setDesc('Enter the token you received from the LogLink Telegram bot here')
+			.addText(text => text
+				.setPlaceholder('telegramabcdefgh123456')
+				.setValue(this.plugin.settings.token)
+
+				.onChange(async (value) => {
+					this.plugin.settings.token = value;
+					await this.plugin.saveSettings();
+				})
+			);
+		}
+
+	}
+
+
+/* function to fetch data from LogLink */
+async function loadRemoteData(userID: string) {
+
+	// Set the URL to either the default or the user's custom URL
+	let remoteAppURL = 'https://api.loglink.it/';
+	let localAppURL = 'http://127.0.0.1:5010/'
+	let appURL = remoteAppURL;
+	let endpoint = appURL + 'get_new_messages/';
+
+	// Crete the results array, which we will use to return the responses
+	let results_array = [];
+
+	console.log(endpoint);
+
+	let object_to_send = {
+		'user_id': userID,
+		'plugin_version': '0.2.1'
+	}
+
+	// Send the request in JSON format to the endpoint
+	let response = await fetch(endpoint, {
+		method: 'POST',
+		headers: {
+			'Accept': 'application/json',
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(object_to_send)
+	});
+
+	let data = await response.json();
+	let status = await response.status;
+
+	if (status === 200) {
+		let messages = data.messages.contents;
+		messages.forEach(function (item, index) {
+			results_array.push(item['contents']);
+		});
+	} else {
+		results_array.push("There was an error fetching your data from LogLink. Error code was " + status + ".");
+		results_array.push(data.message)
+		console.log(data);
+	}
+
+	return results_array;
+}
+
+
+
+
+export default class LoglinkPlugin extends Plugin {
+	settings: LoglinkSettings;
 
 	async onload() {
 		await this.loadSettings();
+		
+		this.addSettingTab(new LoglinkSettingTab(this.app, this));
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
+		
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
 			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			name: 'Get data from LogLink',
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				// Check if the user has entered a token
+				if (this.settings.token === '') {
+					new Notice("❌ You haven't entered a LogLink token. Please enter a token in the settings.")
+					return;
 				}
+
+				new Notice("♻︎ Fetching data from LogLink... with token " + this.settings.token)
+
+				let results = await loadRemoteData(this.settings.token);
+				new Notice ("✅ Data fetched from LogLink")
+
+				// Format the message
+				let base_message = "Inserted from [[LogLink]] on " + new Date().toLocaleString() + ":\n"
+				
+				// Go through each message and add it to the base message with a - at the start
+				let fullmessage = base_message
+				results.forEach(function (item, index) {
+					fullmessage = fullmessage + "- " + item + "\n"
+				});
+
+				editor.replaceSelection(fullmessage)
+				
 			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
@@ -107,28 +167,3 @@ class SampleModal extends Modal {
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
